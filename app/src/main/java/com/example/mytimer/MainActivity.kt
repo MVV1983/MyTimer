a49e05179852379cefbb119da45dcc4803d7721a
v.matyukhin <silf083@gmail.com>
2021-05-29 13:49:57 +0700

    add push

--- app/src/main/java/com/example/mytimer/MainActivity.kt
+++ app/src/main/java/com/example/mytimer/MainActivity.kt
@@ -4,6 +4,8 @@ import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
@@ -17,6 +19,9 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    private val CHANNEL_ID = "Test notif"
+    private val notificationId = 101
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,7 +32,6 @@ class MainActivity : AppCompatActivity() {
         val timeSecond = findViewById<TextView>(R.id.secondView)
         val timerAlarmMinute = findViewById<EditText>(R.id.editTimerMinute)
         val timerAlarmHour = findViewById<EditText>(R.id.editTimerHour)
-
 
 
         object : Thread() {
@@ -48,6 +52,7 @@ class MainActivity : AppCompatActivity() {
             }
         }.start()
 
+        createNotificationChannel()
 
     }
 
@@ -61,12 +66,47 @@ class MainActivity : AppCompatActivity() {
         val checkM = timerAlarmMinute.text.toString()
 
         if (hourNow == checkH && minNow == checkM) {
+
             Toast.makeText(
                 this, "Din-dong :  Wake up ",
                 Toast.LENGTH_SHORT
-            ).show();
-
+            ).show()
 
         }
     }
+
+    fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "Title notification"
+            val descriptionText = "Notif dicsript"
+            val importance: Int = NotificationManager.IMPORTANCE_DEFAULT
+            val channel: NotificationChannel =
+                NotificationChannel(CHANNEL_ID, name, importance).apply {
+                    description = descriptionText
+                }
+
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun sendNotif() {
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_android_black_24dp)
+            .setContentTitle("Уведомление будильника")
+            .setContentText("Wake up")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        with(NotificationManagerCompat.from(this)) {
+            notify(notificationId, builder.build())
+        }
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        sendNotif()
+
+    }
+
 }
